2d4a337cce214bf82e5b35edb7ee4ce4f9811382
Камышев Артем <a.kamyshev@dssl.ru>
2024-10-13 19:33:50 +0300

    Fix EarthC2160.bat script

--- rebalancing_src/Tools/EarthC2160.bat
+++ rebalancing_src/Tools/EarthC2160.bat
@@ -3,8 +3,7 @@ set DEL_PARAM=
 if "%OS%" == "Windows_NT" set DEL_PARAM=/F
 Echo Compiling...
 Rem cl.exe is a compiler from VC++, you can replace it with cpp from gcc
-Rem cl.exe /nologo /E %CL_PARAM% %1 > %1-preprocessed
+cl.exe /nologo /E %CL_PARAM% %1 > %1-preprocessed
 Rem \Games\Earth2160_SDK\Tools\gcc\bin\cpp.exe %1 > %1-preprocessed
-copy %1 %1-preprocessed
 if not errorlevel 1 \Games\Earth2160_SDK\Tools\EarthC2160.exe -w- -nologo -noresult %3 %4 %1-preprocessed %2
 del %DEL_PARAM% %1-preprocessed
